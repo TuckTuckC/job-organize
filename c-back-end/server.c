e627f8e3c46ad6665d47602129bc958beda77f9e
Tucker Craig <tuckermcraig@gmail.com>
2025-12-03 20:26:00 -0500

    adds commands to c server

--- c-back-end/server.c
+++ c-back-end/server.c
@@ -2,16 +2,28 @@
 #include <string.h>
 #include <lmdb.h>
 
-#define GET_USERS 1
-
 int main () {
     char command[50];
     scanf("%s", command);
 
     while(strcmp(command, "quit") != 0) {
+        printf("Command: %s\n", command);
+
         if(strcmp(command, "getUsers") == 0) {
+            printf("calling getUsers...\n");
             // getUsers();
-        };
+        }
+        else if(strcmp(command, "getCompanies") == 0) {
+            printf("calling getCompanies...\n");
+            // getUsers();
+        }
+        else if(strcmp(command, "getUserByID") == 0) {
+            printf("calling getUserByID...\n");
+            // getUsers();
+        }
+
+        scanf("%s", command);
     };
+
     return 0;
 };
