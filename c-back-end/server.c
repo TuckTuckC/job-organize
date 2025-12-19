4747e62bfc48c3a47330aa3681e7cd9bd6a8c96f
Tucker Craig <tuckermcraig@gmail.com>
2025-12-18 22:11:36 -0500

    rewrites c backend to he built fr9m scratch

--- c-back-end/server.c
+++ c-back-end/server.c
@@ -1,9 +1,22 @@
 #include <stdio.h>
 #include <string.h>
-#include <lmdb.h>
+// #include <lmdb.h>
+
+int readFile(char *filePath) {
+    char line[100];
+    FILE *fptr;
+
+    fptr = fopen(filePath, "r");
+
+    while (fgets(line, 100, fptr)) {
+        printf("%s", line);
+    };
+    return 0;
+};
 
 int main () {
     char command[50];
+    printf("Welcome to Job Organize\nEnter a command:\n");
     scanf("%s", command);
 
     while(strcmp(command, "quit") != 0) {
@@ -13,13 +26,22 @@ int main () {
             printf("calling getUsers...\n");
             // getUsers();
         }
+
         else if(strcmp(command, "getCompanies") == 0) {
             printf("calling getCompanies...\n");
             // getUsers();
         }
+
         else if(strcmp(command, "getUserByID") == 0) {
             printf("calling getUserByID...\n");
             // getUsers();
+        }
+
+        else if(strcmp(command, "readFile") == 0) {
+            char filePath[100];
+            printf("Enter the file path: \n");
+            scanf("%s", filePath);
+            readFile(filePath);
         }
 
         scanf("%s", command);
